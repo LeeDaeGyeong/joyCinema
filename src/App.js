49601afb8875fc9d7400f9165c18febc3b140edf
LeeDaeGyeong <dleorud1027@gmail.com>
2024-08-06 22:13:29 +0900

    feat >> style 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import router from "route/route";
+import GlobalStyles from "style/global.style";
+import theme from "style/theme.style";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <RouterProvider router={router}/>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/style/theme.style.js
@@ -0,0 +1,31 @@
+const COLORS = {
+    primary: {
+      yellow: "#FFDE59",
+      orange: "#FB8F4C",
+      green: "#00BF63"
+    },
+    font: "#0F0F0F",
+    error: "#EC0707",
+    white: "#FFFFFF",
+    black: "#0F0F0F",
+  };
+  
+  const FONT_SIZE = {
+    small: "14px",
+    medium: "18px",
+    large: "22px",
+  };
+  
+  const FONT_WEIGHT = {
+    thin: 400,
+    regular: 600,
+    bold: 800,
+  };
+  
+  const theme = {
+    COLORS,
+    FONT_SIZE,
+    FONT_WEIGHT,
+  };
+
+export default theme;
